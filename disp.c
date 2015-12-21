3b12aecf33af5f3ea4ed3071ffc86f80837422d3
volldell <noriaki.taguchi@gmail.com>
2015-12-21 13:54:56 +0900

    change disp

--- disp.c
+++ disp.c
@@ -3,9 +3,9 @@
 
 void message(player p[],int i,int dice_num)
 {
-  printf("Player%d Turn\n", i+1);
+  printf("Player %d Turn\n", i+1);
   printf("Your helth is %d\n",p[i].helth);
-  printf("Your remainig num : %d\n",dice_num);
+  printf("Your remaining num : %d\n",dice_num);
 }
 
 
@@ -18,29 +18,29 @@ void field_disp(int field[][WIDTH], int player)
       switch (field[i][j])
 	{
 	case EMPTY :
-	  printf("\e[0mo");
+	  printf("\e[0mo ");
 	  break;
 	case 1 :
-	  if(player == 0) printf("\e[31m1");
-	  else	          printf("\e[0m1");
+	  if(player == 0) printf("\e[31m1 ");
+	  else	          printf("\e[0m1 ");
 	  break;
 	case 2:
-	  if(player == 1) printf("\e[31m2");
-	  else	          printf("\e[0m2");
+	  if(player == 1) printf("\e[31m2 ");
+	  else	          printf("\e[0m2 ");
 	  break;
 	case 3:
-	  if(player == 2) printf("\e[31m3");
-	  else	          printf("\e[0m3");
+	  if(player == 2) printf("\e[31m3 ");
+	  else	          printf("\e[0m3 ");
 	  break;
 	case 4:
-	  if(player == 3) printf("\e[31m4");
-	  else	          printf("\e[0m4");
+	  if(player == 3) printf("\e[31m4 ");
+	  else	          printf("\e[0m4 ");
 	  break;
 	case M_TOWER :
-		printf("i");
+		printf("\e[0mT ");
 		break;
 	case S_TOWER :
-		printf("l");
+		printf("\e[0mi ");
 		break;
 	default:
 	  break;
